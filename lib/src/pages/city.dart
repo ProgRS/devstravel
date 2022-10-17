92a4665adddbb09fa7b84addaa198ae90de377ce
ProgRs <lfernandotc@hotmail.com>
2022-10-17 12:48:05 -0300

    Implementação tela cidades 4

--- lib/src/pages/city.dart
+++ lib/src/pages/city.dart
@@ -15,6 +15,10 @@ class CityPage extends StatelessWidget{
       fontFamily: 'Helvetica Neue'
   );
 
+  void backButtonAction(pageContext){
+    Navigator.pop(pageContext);
+  }
+
   @override
   Widget build(BuildContext context){
 
@@ -32,20 +36,58 @@ class CityPage extends StatelessWidget{
           backgroundColor: Colors.white,
           body: Stack(
             children: <Widget> [
-                Container(
-                  height: 250,
-                  decoration: BoxDecoration(
-                     color: Colors.red
-                  ),
-                ),
+              Positioned(
+                  top: 0,
+                   left: 0,
+                   right: 0,
+                   height: 250,
+                   child: Image.network(
+                     cityData['places'][0]['img'],
+                     fit: BoxFit.cover,
+                   ),
+              ),
+
+
+              ListView(
+                physics: ClampingScrollPhysics(),
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                     Container(
+                       margin: EdgeInsets.only(top: 220),
+                         decoration: BoxDecoration(
+                             color: Colors.white,
+                             borderRadius: BorderRadius.circular(20)
+                         ),
+                       child: Column(
+                                children: < Widget >[
+                                  Row(children: <Widget>[
+                                        Column(
+                                          children: <Widget>[
+                                            Text(cityData['name'], style: TextStyle(
+                                                fontFamily: 'Helvetica Neue',
+                                                fontSize: 19,
+                                                fontWeight: FontWeight.bold
+                                            ),),
+                                            Text('Estrelinhas')
+                                          ],
+                                        ),
+                                    Text('...')
+                                  ],)
+                                ],
+                       ),
+                     ),
+                ],
+              ),
               Container(
                 height: 50,
                 margin: EdgeInsets.only(top:statusBarHeight),
                 child: IconButton(
                   icon: Icon(Icons.arrow_back, color: Colors.white,),
-                  onPressed: () {  },
+                  onPressed: (){
+                    backButtonAction(context);
+                  },
                 ),
-              )
+              ),
             ],
           )
         )
